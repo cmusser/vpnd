ec46ddd6190a4e0ebc7ebe0aeb319d31afe4a91c
Chuck Musser <cmusser@sonic.net>
2018-03-27 22:01:51 -0700

    Rename OS module to be platform specific; bump version
    The OS module is now explicitly BSD specific.

--- vpnd.h
+++ vpnd.h
@@ -18,7 +18,7 @@
 
 #include "uthash.h"
 
-#define VPND_VERSION "1.1.2-b1"
+#define VPND_VERSION "1.1.2"
 
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
